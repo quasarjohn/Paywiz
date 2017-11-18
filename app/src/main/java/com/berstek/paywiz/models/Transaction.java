3bce290bf4b7066abf9e39035c383117e0e70ba8
quasarjohn <quasarjohn21@gmail.com>
2017-11-18 01:17:39 -0800

    Layout for transactions. TODO use layout from jarvs

---
+++ app/src/main/java/com/berstek/paywiz/models/Transaction.java
@@ -0,0 +1,23 @@
+package com.berstek.paywiz.models;
+
+/**
+ * Created by Berstek on 11/18/2017.
+ */
+
+public class Transaction {
+
+    private String title, detail;
+    private String seller_uid, buyer_uid;
+    private String courier, address;
+    private TransactionType transaction_type;
+    private long creation_date, expiration_date, shipment_date;
+    private double amount, trans_charge, percent_charge;
+
+    public enum TransactionType {
+        DOOR, PICKUP
+    }
+
+    public enum Status {
+        AWAITING_ACCEPTANCE, AWAITING_SHIPMENT, COMPLETED
+    }
+}
--- app/src/main/java/com/berstek/paywiz/views/transactions/TransactionsFragment.java
+++ app/src/main/java/com/berstek/paywiz/views/transactions/TransactionsFragment.java
@@ -3,17 +3,24 @@ package com.berstek.paywiz.views.transactions;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.berstek.paywiz.R;
+import com.berstek.paywiz.models.Transaction;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TransactionsFragment extends Fragment {
 
+    private View view;
+    private RecyclerView recyclerView;
 
     public TransactionsFragment() {
         // Required empty public constructor
@@ -24,7 +31,24 @@ public class TransactionsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transactions, container, false);
+        view = inflater.inflate(R.layout.fragment_transactions, container, false);
+        recyclerView = view.findViewById(R.id.recview_transactions);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        //TODO fetch data from database
+
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        Transaction transaction = new Transaction();
+
+        for (int i = 0; i < 20; i++) {
+            transactions.add(transaction);
+        }
+
+        TransactionsAdapter adapter = new TransactionsAdapter(getContext(), transactions);
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 
 }
