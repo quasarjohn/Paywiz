790bbca78d7393dc6f446e087cc6fe561e42316c
rm-RedHummer <rm.RedHummer@github.com>
2017-11-21 22:59:50 +0800

    wiring feedbacks adapter

--- app/src/main/java/com/berstek/paywiz/views/feedback/FeedbacksAdapter.java
+++ app/src/main/java/com/berstek/paywiz/views/feedback/FeedbacksAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.berstek.paywiz.R;
 import com.berstek.paywiz.models.Feedback;
@@ -17,6 +19,7 @@ public class FeedbacksAdapter extends RecyclerView.Adapter<FeedbacksAdapter.List
     private ArrayList<Feedback> feedbacks;
     private LayoutInflater inflater;
 
+
     public FeedbacksAdapter(Context context, ArrayList<Feedback> feedbacks) {
         this.context = context;
         this.feedbacks = feedbacks;
@@ -27,12 +30,17 @@ public class FeedbacksAdapter extends RecyclerView.Adapter<FeedbacksAdapter.List
     @Override
     public ListHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = inflater.inflate(R.layout.viewholder_feedback, parent, false);
+
         return new ListHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ListHolder holder, int position) {
         //TODO
+        if(holder.getAdapterPosition()==1)
+            holder.name.setText("Reymark Arsenio");
+        else
+            holder.name.setText("Danmark Arqueza");
     }
 
     @Override
@@ -41,9 +49,16 @@ public class FeedbacksAdapter extends RecyclerView.Adapter<FeedbacksAdapter.List
     }
 
     class ListHolder extends RecyclerView.ViewHolder {
+        private ImageView dp,rating;
+        private TextView description, name, comment;
 
         public ListHolder(View itemView) {
             super(itemView);
+        dp = itemView.findViewById(R.id.profile_image);
+        rating = itemView.findViewById(R.id.rating_imageview);
+        description = itemView.findViewById(R.id.description_textview);
+        name = itemView.findViewById(R.id.name_textview);
+        comment = itemView.findViewById(R.id.comment_textview);
         }
     }
 }
