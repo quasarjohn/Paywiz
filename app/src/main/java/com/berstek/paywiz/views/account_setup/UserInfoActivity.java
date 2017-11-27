d3ae65237f51a37b57384efa09fd303444e43fcc
quasarjohn <quasarjohn21@gmail.com>
2017-11-27 13:51:11 +0800

    User info activity

---
+++ app/src/main/java/com/berstek/paywiz/views/account_setup/UserInfoActivity.java
@@ -0,0 +1,15 @@
+package com.berstek.paywiz.views.account_setup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.berstek.paywiz.R;
+
+public class UserInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}
